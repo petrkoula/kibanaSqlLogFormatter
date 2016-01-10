2557653bedfd72eacae41f61af67b1277fe10667
Petr Koula <petr.koula@unicornsystems.eu>
2016-01-10 17:18:43 +0100

    replace function fixed to replace all occurrences of the parameter in the query

--- formatter.js
+++ formatter.js
@@ -2,19 +2,24 @@ javascript: (function() {
   var getFieldElement = function(fieldName) {
       return $("doc-viewer table td[title='%FIELD%'] + td > div".replace("%FIELD%", fieldName));
   };
-  var formatSql = function(query, params) {
-    if (!query || !sqlParametersJson) throw 'Sql or SqlParameters cannot be empty';
+  var formatSql = function (query, params) {
+      if (!query) return "";
+      params = params || []; 
+      
+      var replaceAll = function (text, patternToReplace, replaceWith) {
+          return text.replace(new RegExp(patternToReplace, "g"), replaceWith);
+      };
+      
+      _.forEach(params, function (p) {
+          var name = p.Name;
+          var value = " " + formatParamValueByType(p.Value) + " /*" + name + "*/";
+          
+          console.log("replacing " + name + " with " + value);
+          query = replaceAll(query, ": " + name, value);
+          query = replaceAll(query, ":" + name, value);
+      });
 
-    _.forEach(params, function (p) {
-        var name = p.Name;
-        var nameCommented = " /*" + name + "*/";
-        var value = p.Value;
-        console.log("replacing " + name + " with " + value);
-        query = query.replace(": " + name, " " + formatParamValueByType(value) + nameCommented);
-        query = query.replace(":" + name, " " + formatParamValueByType(value) + nameCommented);
-    });
-
-    return query;
+      return query;
   };
   var matchDate = function(input) {
     var iso = /^(\d{4}\-\d\d\-\d\d([tT][\d:\.]*)?)([zZ]|([+\-])(\d\d):(\d\d))?$/;
@@ -33,8 +38,7 @@ javascript: (function() {
     var sqlParametersJson = getFieldElement('SqlParamaters').text();
     var sqlParameters = JSON.parse("[" + sqlParametersJson + "]");
     
-    var formattedSql = formatSql(sqlElement.text(), sqlParameters)
-    
+    var formattedSql = formatSql(sqlElement.text(), sqlParameters);
     sqlElement.text(formattedSql);
     alert("Sql query formatted with parameters.");
     
